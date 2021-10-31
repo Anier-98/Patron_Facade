4e415733349a52eb8ababae11a13a31779543ac1
Anier-98 <anierf3@gmail.com>
2021-10-30 22:49:03 -0600

    llamadas a las funciones

--- Facade/src/main/java/org/anier/PruebaPatron.java
+++ Facade/src/main/java/org/anier/PruebaPatron.java
@@ -5,15 +5,14 @@ import java.io.*;
 public class PruebaPatron {
 
     public static void main(String args[]) throws IOException {
-        BufferedReader entrada = _____________________________;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
         FachadaCajero fachada = new FachadaCajero();
-        System.out.println("Introduce operación cajero \n1- Sacar Dinero \n2- Recarga
-                Móvil \nOpción:");
-        int opcion = Integer.parseInt(______________________________);
+        System.out.println("Introduce operación cajero \n1- Retirar Dinero \n2- Recarga Móvil \nOpción:");
+        int opcion = Integer.parseInt(entrada.readLine());
         if (opcion == 1) {
-            System.out.println(_____________________________);
+            System.out.println(fachada.sacarDinero());
         } else {
-            System.out.println(__________________________________);
+            System.out.println(fachada.recargaMovil());
         }
     }
 }
