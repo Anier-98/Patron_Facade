8ede0d8a442c8948a7535fab18899fef6a7a100b
Anier-98 <anierf3@gmail.com>
2021-10-30 21:55:51 -0600

    creacion de las clases validacion y prueba

---
+++ Facade/src/main/java/org/anier/PruebaPatron.java
@@ -0,0 +1,19 @@
+package org.anier;
+
+import java.io.*;
+
+public class PruebaPatron {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader entrada = _____________________________;
+        FachadaCajero fachada = new FachadaCajero();
+        System.out.println("Introduce operación cajero \n1- Sacar Dinero \n2- Recarga
+                Móvil \nOpción:");
+        int opcion = Integer.parseInt(______________________________);
+        if (opcion == 1) {
+            System.out.println(_____________________________);
+        } else {
+            System.out.println(__________________________________);
+        }
+    }
+}
---
+++ Facade/src/main/java/org/anier/ValidacionUsuario.java
@@ -0,0 +1,8 @@
+package org.anier;
+
+public class ValidacionUsuario {
+    public String valida() {
+        return "\n### Validar usuario:"
+                + "\n\tValidación del cliente...";
+    }
+}
